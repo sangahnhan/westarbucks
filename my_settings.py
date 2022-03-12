671811f9ac9247906dac16adeee9f2c5dc06c7c5
sangahnhan <sangahn0524@gmail.com>
2022-03-12 16:30:45 +0900

    Add: Django Project Setting

---
+++ my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks',
+        'USER': 'root',
+        'PASSWORD': '23571113',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+SECRET_KEY = 'django-insecure-k1ruom=-3+ewvnk-+c6ai1-wg+^zm_=x@)zwz*s%htxq&x+-f$'
